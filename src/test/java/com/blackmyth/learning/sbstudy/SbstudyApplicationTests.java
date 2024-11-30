ffd727742ea909b65d7db04c1c6ecf8a326c0985
hcucu <hcucu@163.com>
2024-11-30 00:07:51 -0500

    first commit

---
+++ src/test/java/com/blackmyth/learning/sbstudy/SbstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackmyth.learning.sbstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
